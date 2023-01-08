ab876322b1779db0510aa682861808bb37c877da
juchajam <79612680+juchajam@users.noreply.github.com>
2023-01-09 07:31:27 +0900

    Update 2_cam.py

--- image_data/2_cam.py
+++ image_data/2_cam.py
@@ -8,17 +8,20 @@ capture = cv2.VideoCapture(0)
 # 영상 가로, 세로 사이즈 설정
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
-
+fps = capture.get(cv2.CAP_PROP_FPS)
+dt = int(1000/fps)
 n = 1
 # 무한반복문
 while True:
     _, frame = capture.read()     # 카메라로부터 현재 영상을 받아 frame에 저장, 잘 받았다면 ret가 참
     cv2.imshow("cam", frame)        # frame(카메라 영상)을 cam 이라는 창에 띄워줌
 
-    if cv2.waitKey(2) == ord('q'):  # 키보드의 q 를 누르면 종료
+    key = cv2.waitKey(dt) & 0xFF
+    
+    if key == ord('q'):  # 키보드의 q 를 누르면 종료
         break
     
-    if cv2.waitKey(2) == ord('p'):  # p 누르면 프레임 캡쳐
+    if key == ord('p'):  # p 누르면 프레임 캡쳐
         file_name = f"capture{n}.jpg"
         img_file = os.path.join(path, file_name)
         cv2.imwrite(img_file, frame) # 현제 폴더에 capure01.jpg로 이미지 저장
