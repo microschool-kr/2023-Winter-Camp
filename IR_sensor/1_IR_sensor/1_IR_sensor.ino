50a60939f173c0cba9781272a5167cde55c9ca4d
juchajam <79612680+juchajam@users.noreply.github.com>
2023-01-09 07:33:47 +0900

    Update 1_IR_sensor.ino

--- IR_sensor/1_IR_sensor/1_IR_sensor.ino
+++ IR_sensor/1_IR_sensor/1_IR_sensor.ino
@@ -19,7 +19,7 @@ void setup() {
   pinMode(IR_M, INPUT);
   pinMode(IR_R, INPUT);
   Serial.begin(9600);
-  Serial.print("Success");
+  Serial.print("Start");
 }
 
 
@@ -33,8 +33,6 @@ void loop() {
   Serial.print(IR_M_data);
   Serial.print("-");
   Serial.println(IR_R_data);
-
-  delay(100);
 }
 
 
